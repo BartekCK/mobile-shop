ebaaa73cf2321029ac9941b880a5a9c2e3085944
Bartłomiej Kotarski <bartlomiej.kotarski@netguru.com>
2021-06-29 19:58:36 +0200

    Add footer to pay place

--- frontend/src/components/nav-list/NavList.tsx
+++ frontend/src/components/nav-list/NavList.tsx
@@ -19,11 +19,23 @@ const NavList: React.FC<Props> = (props: Props) => {
 
   const products = useSelector(Product.getProducts);
 
+  const totalPrice: number = React.useMemo(() => {
+    if(!products.length) return 0;
+
+    return products.reduce((prev, curr) => curr.price * curr.count + prev,0)
+  },[products])
+
   return (
     <div className={`nav-list--wrapper ${isOpen ? 'visible' : ''}`}>
       {products.map((product) => (
         <Item key={product.id} product={product} />
       ))}
+      <div className="footer">
+        <span className="total-price">Total price: {totalPrice.toFixed(2)} $</span>
+        <button type="button" className="btn btn-secondary" disabled={totalPrice === 0}>
+          Pay now
+        </button>
+      </div>
     </div>
   );
 };
